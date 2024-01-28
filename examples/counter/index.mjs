285bed5e9ec2cc447094faa23fcda976f8f89708
0scarB <oscarb@protonmail.com>
2024-01-28 17:08:46 +0100

    Add counter example

--- examples/counter/index.mjs
+++ examples/counter/index.mjs
@@ -1,16 +1,3 @@
-# Grug-UI
-
-Grug-UI is a set of light-weight JavaScript utilities for programmatically:
-- Generating HTML and CSS -- SSR
-- Creating DOM and CSSDOM (TBD) nodes
-- Mutating the DOM (TBD)
-- Enabling interactivity
-
-
-## Examples
-
-### [Counter](./examples/counter/index.mjs)
-```js
 import {beginExec} from "./grugui.mjs";
 
 let count = 0;
@@ -46,17 +33,4 @@ function update() {
 }
 
 update();
-```
-
-### [Tic-Tac-Toe](./examples/tic-tac-toe/index.mjs)
-
-
-## TODOs
-
-- [ ] Add HTML statements set for DOM mutation
-- [ ] Add CSS statements as wrapper around the CSSStyleSheet API
-- [ ] Provide API for creation of reactive "components"
-    - More or less just just add something that emulates an event loop
-      by calling an `update` function
-- [ ] Add JSDoc and generate `types.d.ts`
 
